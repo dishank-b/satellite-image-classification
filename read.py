d3e4b4bb69de8ed69dcf03f3d5e62ef645e23d4d
americast <sayan.sinha@iitkgp.ac.in>
2018-11-02 16:27:11 +0530

    Save data

--- read.py
+++ read.py
@@ -5,46 +5,52 @@ import cv2
 import pudb
 
 classes = {
-	'[0. 0. 0.]': 0,
-	'[  0.  0. 150.]': 1,
-	'[  0. 125.   0.]': 2,
-	'[  0. 255.   0.]': 3,
-	'[100. 100. 100.]': 4, 
-	'[150.  80.   0.]': 5,
-	'[150. 150. 255.]': 6,
-	'[255. 255.   0.]': 7,
-	'[255. 255. 255.]': 8
+	'[0 0 0]': 0,
+	'[  0  0 150]': 1,
+	'[  0   0 150]': 1,
+	'[  0 125   0]': 2,
+	'[  0 255   0]': 3,
+	'[100 100 100]': 4, 
+	'[150  80   0]': 5,
+	'[150 150 255]': 6,
+	'[255 255   0]': 7,
+	'[255 255 255]': 8
 
 }
-
-org_file = "data/sat/3.tif"
-gt_file = "data/gt/3.tif"
-
-org_data = rasterio.open(org_file)
-gt_data = rasterio.open(gt_file)
-
-r, g, b, ir = org_data.read()
-gt_r, gt_g, gt_b = gt_data.read()
-r, g, b, ir = r.astype(float), g.astype(float), b.astype(float), ir.astype(float)
-gt_r, gt_g, gt_b = gt_r.astype(float), gt_g.astype(float), gt_b.astype(float)
-
-r /= np.max(r)
-g/= np.max(g)
-b/= np.max(b)
-ir /= np.max(ir)
-
-r, g, b, ir = r.flatten(), g.flatten(), b.flatten(), ir.flatten()
-gt_r, gt_g, gt_b = gt_r.flatten(), gt_g.flatten(), gt_b.flatten()
-
 org = []
 gt = []
+file = [str(i) for i in range(1,14)]
 
-for i in range(len(r)):
-	org.append([r[i], g[i], b[i], ir[i]])
-	gt.append([gt_r[i], gt_g[i], gt_b[i]])
+for f in file:
+	print(f)
+	org_file = "data/sat/"+f+".tif"
+	gt_file = "data/gt/"+f+".tif"
+
+	org_data = rasterio.open(org_file)
+	gt_data = rasterio.open(gt_file)
+
+	r, g, b, ir = org_data.read()
+	gt_r, gt_g, gt_b = gt_data.read()
+	r, g, b, ir = r.astype(float), g.astype(float), b.astype(float), ir.astype(float)
+
+	r /= np.max(r)
+	g/= np.max(g)
+	b/= np.max(b)
+	ir /= np.max(ir)
+
+	r, g, b, ir = r.flatten(), g.flatten(), b.flatten(), ir.flatten()
+	gt_r, gt_g, gt_b = gt_r.flatten(), gt_g.flatten(), gt_b.flatten()
+
+
+	for i in range(len(r)):
+		org.append([r[i], g[i], b[i], ir[i]])
+		temp = np.array([gt_r[i], gt_g[i], gt_b[i]])
+		gt.append(classes[str(temp)])
 
 org = np.array(org)
 gt = np.array(gt)
+np.save("X.npy", org)
+np.save("Y.npy", gt)
 pu.db
 
 # col_img = P[: , :, 0:3]
